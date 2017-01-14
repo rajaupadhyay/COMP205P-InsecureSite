d2ec68c2e90fca38d4037cfc65813a16abc3fcfa
Sidharth Sikka <sidharthsikka@users.noreply.github.com>
2017-01-14 00:36:39 +0000

    removed security data

--- constants.php
+++ constants.php
@@ -1,8 +1,8 @@
 <?php 
-	define('DB_NAME', 'vulnerable site');
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', 'sidharth12468');
-	define('DB_HOST', 'localhost');
+	define('DB_NAME', '');
+	define('DB_USER', '');
+	define('DB_PASSWORD', '');
+	define('DB_HOST', '');
 
 	$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
 	if(!$link){
