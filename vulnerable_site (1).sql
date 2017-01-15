343e631282a5f8e101e3520859da8676776c3a1e
Sidharth Sikka <sidharthsikka@users.noreply.github.com>
2017-01-15 01:04:51 +0000

    updated database for default image with root acc

---
+++ vulnerable_site (1).sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 15, 2017 at 01:04 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vulnerable site`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `files`
+--
+
+CREATE TABLE `files` (
+  `ID` int(11) NOT NULL,
+  `fileURL` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `userID` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `snippets`
+--
+
+CREATE TABLE `snippets` (
+  `ID` int(11) NOT NULL,
+  `Text` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `userID` int(11) NOT NULL,
+  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `Username` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `Email` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `Password` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `URL` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'index.php',
+  `imageURL` varchar(5000) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'https://openclipart.org/image/800px/svg_to_png/215813/Penguin-Icon.png',
+  `color` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `privateSnippet` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Admin` tinyint(1) NOT NULL DEFAULT '0',
+  `snippetAccess` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID`, `Username`, `Email`, `Password`, `URL`, `imageURL`, `color`, `privateSnippet`, `Admin`, `snippetAccess`) VALUES
+(1, 'root', 'root@gmail.com', 'root', 'index.php', 'https://openclipart.org/image/800px/svg_to_png/215813/Penguin-Icon.png', NULL, NULL, 1, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `snippets`
+--
+ALTER TABLE `snippets`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Username` (`Username`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `snippets`
+--
+ALTER TABLE `snippets`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
